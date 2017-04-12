cac7baccd78a3da052dea006d88a3968314dce4a
s77848 <johan.oberg@seb.se>
2017-04-12 14:47:25 +0200

    Ny func gjord

---
+++ ConsoleApp/ConsoleApp/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp {
+	public class Person {
+
+		public Person() {
+			Name = "Test";
+			Adress = "Testvägen 12";
+		}
+
+		public string Name { get; set; }
+		public string Adress { get; set; }
+	}
+}
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,14 +7,21 @@ using System.Threading.Tasks;
 namespace ConsoleApp {
 	class Program {
 		static void Main(string[] args) {
-			System.Diagnostics.Debug.WriteLine("test");
-			System.Diagnostics.Debug.WriteLine("test git");
-			System.Diagnostics.Debug.WriteLine("change more");
+			System.Diagnostics.Debug.Print("test");
+			System.Diagnostics.Debug.Print("test git");
+			System.Diagnostics.Debug.Print("change more");
 			PrintMoreStuff();
+			PrintPerson();
 		}
 
 		private static void PrintMoreStuff() {
-			System.Diagnostics.Debug.WriteLine("Made in Johan Branch");
+			System.Diagnostics.Debug.Print("Made in Johan Branch");
 		}
+
+		private static void PrintPerson() {
+			var p = new Person();
+			System.Diagnostics.Debug.Print(string.Format("{0}, {1}", p.Name, p.Adress));
+		}
+
 	}
 }
